143d37924b9a84aa64f31f41f2f047809984f0c9
Ayush Ranjan Roy <73477003+ayushranjanroy@users.noreply.github.com>
2022-09-07 21:55:18 +0530

    Time: 495 ms (27.73%), Space: 25.4 MB (75.86%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -1,5 +1,5 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
         nums[:] = nums[-(k % len(nums)):] + nums[:-(k % len(nums))]
-        # find the pivot and reverse both arrays
+        # find the pivot and reverse both array slices
         
