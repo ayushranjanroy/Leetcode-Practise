4a377e5844b23c304e780263f08f7c4b03b0d881
Ayush Ranjan Roy <73477003+ayushranjanroy@users.noreply.github.com>
2022-09-07 20:48:12 +0530

    Time: 453 ms (13.84%), Space: 123.8 MB (94.36%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -1,13 +1,18 @@
-class Solution:
-    def rearrangeArray(self, nums: List[int]) -> List[int]:
-        nex=1
-        cur=0
-        ans = [0]*len(nums)
-        for i in nums:
-            if "-" in str(i):
-                ans[nex] = i
-                nex += 2
-            else:
-                ans[cur] = i
-                cur += 2
-        return ans
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int> ans(nums.size(),0);
+        int cur = 0; int nex = 1;int i=0;
+        for(auto it:nums){
+            if(it<0){
+                ans[nex]=it;
+                nex+=2;
+            }
+            else if(it>0){
+                ans[cur]=it;
+                cur+=2;
+            }
+        }
+        return ans;
+    }
+};
